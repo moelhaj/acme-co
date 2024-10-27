6406db2c0e683ceaf8ee9893bc0a861158439ae1
Mohamed Elhaj <mo.elhaj@outlook.com>
2024-10-27 21:32:57 +0400

    fixing build issues

--- src/actions/users.ts
+++ src/actions/users.ts
@@ -1,6 +1,3 @@
 "use server";
-// import { unstable_noStore as noStore } from "next/cache";
-// import { revalidatePath } from "next/cache";
-// import { redirect } from "next/navigation";
 
 export async function getUsers() {}
